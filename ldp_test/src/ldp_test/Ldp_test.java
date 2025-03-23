d16e11746fce759044187f956546baba6ffd94cc
BBotelho30 <beambotelho11@gmail.com>
2025-03-23 17:37:06 +0000

    javacommit

---
+++ ldp_test/src/ldp_test/Ldp_test.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ldp_test;
+
+/**
+ *
+ * @author sofia
+ */
+public class Ldp_test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
